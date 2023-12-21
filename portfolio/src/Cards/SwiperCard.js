90dc26f6790f321946617b9061bed86559e1673a
Shahad Hassan <shahadg1983@gmail.com>
2023-12-21 15:26:54 +0530

    Adding scripts

--- portfolio/src/Cards/SwiperCard.js
+++ portfolio/src/Cards/SwiperCard.js
@@ -7,49 +7,49 @@ import 'swiper/css/navigation';
 import { Autoplay, Navigation } from 'swiper/modules';
 
 export default function SwiperCard() {
-  return (
-    <>
-      <Swiper
-        spaceBetween={30}
-        centeredSlides={true}
-        autoplay={{
-          delay: 4500,
-          disableOnInteraction: false,
-        }}
-        navigation={true}
-        modules={[Autoplay, Navigation]}
-        className="mySwiper flex justify-center align-items-center">
-                    <SwiperSlide className="flex text-center flex-col justify-items-center align-content-center ">
-                        <p className="mb-4 w-2/3 self-center text-justify text-xl">
-                            "I have always believed, and I still believe, that whatever good or bad fortune may come our way, we can always give it meaning and transform it into something of value."
-                        </p>
-                        <div className="self-center">
-                            <img src = {Pic} alt='Mentor-Pic' className=" w-16" />
-                        </div>
-                        <h3 className=" font-bold">Shahad Hassan</h3>
-                        <p>Developer</p>
-                    </SwiperSlide>
-                    <SwiperSlide className="flex text-center flex-col justify-items-center align-content-center ">
-                        <p className="mb-4 w-2/3 self-center text-justify text-xl">
-                            "I have always believed, and I still believe, that whatever good or bad fortune may come our way, we can always give it meaning and transform it into something of value."
-                        </p>
-                        <div className="self-center">
-                            <img src = {Pic} alt='Mentor-Pic' className=" w-16" />
-                        </div>
-                        <h3 className=" font-bold">Shahad Chauhan</h3>
-                        <p>Developer</p>
-                    </SwiperSlide>
-                    <SwiperSlide className="flex text-center flex-col justify-items-center align-content-center ">
-                        <p className="mb-4 w-2/3 self-center text-justify text-xl">
-                            "I have always believed, and I still believe, that whatever good or bad fortune may come our way, we can always give it meaning and transform it into something of value."
-                        </p>
-                        <div className="self-center">
-                            <img src = {Pic} alt='Mentor-Pic' className=" w-16" />
-                        </div>
-                        <h3 className=" font-bold">Shahad Gurjar</h3>
-                        <p>Developer</p>
-                    </SwiperSlide>
-      </Swiper>
-    </>
-  );
+    return (
+        <>
+            <Swiper
+            spaceBetween={30}
+            centeredSlides={true}
+            autoplay={{
+            delay: 4500,
+            disableOnInteraction: false,
+            }}
+            navigation={true}
+            modules={[Autoplay, Navigation]}
+            className="mySwiper flex justify-center align-items-center">
+                <SwiperSlide className="flex text-center flex-col justify-items-center align-content-center ">
+                    <p className="mb-4 w-2/3 self-center text-justify text-xl">
+                        "I have always believed, and I still believe, that whatever good or bad fortune may come our way, we can always give it meaning and transform it into something of value."
+                    </p>
+                    <div className="self-center">
+                        <img src = {Pic} alt='Mentor-Pic' className=" w-16" />
+                    </div>
+                    <h3 className=" font-bold">Shahad Hassan</h3>
+                    <p>Developer</p>
+                </SwiperSlide>
+                <SwiperSlide className="flex text-center flex-col justify-items-center align-content-center ">
+                    <p className="mb-4 w-2/3 self-center text-justify text-xl">
+                        "I have always believed, and I still believe, that whatever good or bad fortune may come our way, we can always give it meaning and transform it into something of value."
+                    </p>
+                    <div className="self-center">
+                        <img src = {Pic} alt='Mentor-Pic' className=" w-16" />
+                    </div>
+                    <h3 className=" font-bold">Shahad Chauhan</h3>
+                    <p>Developer</p>
+                </SwiperSlide>
+                <SwiperSlide className="flex text-center flex-col justify-items-center align-content-center ">
+                    <p className="mb-4 w-2/3 self-center text-justify text-xl">
+                        "I have always believed, and I still believe, that whatever good or bad fortune may come our way, we can always give it meaning and transform it into something of value."
+                    </p>
+                    <div className="self-center">
+                        <img src = {Pic} alt='Mentor-Pic' className=" w-16" />
+                    </div>
+                    <h3 className=" font-bold">Shahad Gurjar</h3>
+                    <p>Developer</p>
+                </SwiperSlide>
+            </Swiper>
+        </>
+    );
 }
